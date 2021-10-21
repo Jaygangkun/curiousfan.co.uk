b65f19ebc4ed8fcf47fd202fba15774cd8df06ac
Jay <jaygangkun@hotmail.com>
2021-10-21 18:08:18 -0400

    fix homepage for client request

--- resources/views/livewire/creators-sidebar.blade.php
+++ resources/views/livewire/creators-sidebar.blade.php
@@ -1,8 +1,12 @@
-<div class="right-sidebar">
-    <div class="row">
-        <div class="col-md-12">
-            <strong style="font-size: 14px;margin-bottom: 10px;display: block;color: #7f7f7f;">SUGGESTIONS</strong>
+<div class="right-sidebar1">
+    <div class="card">
+        <div class="card-body pt-2 pb-0">
+            <div class="row">
+                <div class="col-md-12">
+                    <strong style="font-size: 14px;margin-bottom: 10px;display: block;color: #7f7f7f;">SUGGESTIONS</strong>
+                </div>
+            </div>
+            @include('creators.loop')
         </div>
     </div>
-    @include('creators.loop')
 </div>
--- resources/views/posts/sidebar-desktop.blade.php
+++ resources/views/posts/sidebar-desktop.blade.php
@@ -1,12 +1,21 @@
 <div class="col-12 col-md-3 d-none d-sm-none d-md-none d-lg-block">
 	<div class="sticky-top">
-	
-	@if( isset($feed) && $feed->count() )
-		<div class="lastId d-none">{{ $feed->last()->id }}</div>
-	@endif
+		<div class="right-sidebar">
+			@if( isset($feed) && $feed->count() )
+				<div class="lastId d-none">{{ $feed->last()->id }}</div>
+			@endif
 
-	@livewire('creators-sidebar')
+			@livewire('creators-sidebar')
 
-	<br>
+			<div class="card mt-3">
+				<div class="card-header">View Your Profile</div>
+				<div class="card-body">
+					<p>Make a great first impression of yourself.</p>
+					<div class="text-center">
+						<a href="/{{ auth()->user()->profile->username }}" class="btn btn-primary" style="padding: 5px 20px 5px 20px; font-size: 16px; border-radius: 16px;">View Profile</a>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
